67e1a4545e83e6c786e68e468bbf2d1f2f2a5321
elbren <elbrenantonio@sksu.edu.ph>
2025-10-03 21:04:21 +0800

    feature/1

--- src/pages/api/slprivate/index.js
+++ src/pages/api/slprivate/index.js
@@ -4,12 +4,12 @@ import Cookies from "js-cookie";
 
 //ITO ANG KUKUHA NG DATA SA DATABASE TABLE NA LIVESTOCK
 //TINAWAG ANG GETLIVESTOCK SA PAGE NA INDEX.JS SA FOLDER NA LIVE_STOCK AT PAGE NA pages
-export async function GetLivestock(body) {
+export async function GetSLPrivate(body) {
  
     const token = Cookies.get('accessToken')
   
     const response =  await axios
-      .get(`${process.env.NEXT_PUBLIC_API_URL}/admin/livestock`,{
+      .get(`${process.env.NEXT_PUBLIC_API_URL}/admin/slprivate`,{
         headers: {
           "Content-Type": "application/json",
           Authorization: `Bearer ${token}`,
@@ -23,7 +23,8 @@ export async function GetLivestock(body) {
 
 
   //ITO ANG MAGPOPOST NG DATA SA DATABASE TABLE NA LIVESTOCK PARANG INSERT NG PROCESS
-export async function postSLPrivate(body) {
+export async function postSLPrivate(body) 
+{
  
     const token = Cookies.get('accessToken')
   
@@ -40,40 +41,24 @@ export async function postSLPrivate(body) {
     
   }
 
+export async function putSLPrivate(body) {
+  const token = Cookies.get("accessToken");
 
+  return await axios.put(`${process.env.NEXT_PUBLIC_API_URL}/admin/slprivate/${body?.id}`, body, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+}
 
-  
-export async function deleteLivestock(id) {
- 
-    const token = Cookies.get('accessToken')
-  
-    const response =  await axios
-      .delete(`${process.env.NEXT_PUBLIC_API_URL}/admin/livestock/${id}`, {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      })
+export async function deleteSLPrivate(id) {
+  const token = Cookies.get("accessToken");
 
-
-      return response
-    
-  }
-
-  export async function putLivestock(body) {
-
- 
-    const token = Cookies.get('accessToken')
-  
-    const response =  await axios
-      .put(`${process.env.NEXT_PUBLIC_API_URL}/admin/livestock/${body?.id}`, body, {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      })
-
-
-      return response
-    
-  }
+  return await axios.delete(`${process.env.NEXT_PUBLIC_API_URL}/admin/slprivate/${id}`, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+}
